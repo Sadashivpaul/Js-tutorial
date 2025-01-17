25e1f196d82451a44d4ce7aa72fa709b50e5427b
Sadashiv Paul <sadashivpaul007@gmail.com>
2025-01-17 14:36:28 +0000

    comparison

---
+++ .devcontainer/Basics/04_comparison.js
@@ -0,0 +1,29 @@
+// console.log(2>1);;
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+// above programs has simple outputs
+
+//lets check for it
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+// in javascript the comparison and equlaity check are the two different things
+
+//=== is the strict check where the data type is also checked
+console.log("2"===2);
+
+
+
+
+
