dda270e7f965b8d4bbd3d82ee3dc02cef82b7a71
Sadashiv Paul <sadashivpaul007@gmail.com>
2025-01-22 18:39:37 +0000

    strings

---
+++ .devcontainer/Basics/05_strings.js
@@ -0,0 +1,43 @@
+ // string can represet in single quote and double quote
+//   const name ="sadashiv"
+//   const repoCount = 50
+
+// //   console.log(name+repoCount+"Value");
+//   console.log(`hello my name is ${name} and my repo count is ${repoCount}`); // recomemnded to use ` `
+
+  const gameName = new String('Sadashiv');
+//   console.log(gameName);
+  
+//   console.log(gameName[0]);
+//   console.log(gameName.__proto__);
+  
+//   console.log(gameName.length);
+//   console.log(gameName.toUpperCase());
+//   console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('v'));
+
+// const newgameName = gameName.substring(0,4) // we can't use negative values here thats why we use slice, if we use negative values by any chance then it avoids the nagative value and take it as a positive
+// console.log(newgameName);
+
+// const anotherString = gameName.slice(-7,4)
+// console.log(anotherString);
+
+
+// const newString1 = "          Sadashiv         "
+// console.log(newString1);
+// console.log(newString1.trim());// By using trim the unwanted spaces are removed
+
+
+// const url ="https://sadashiv.com/sadshiv%20paul" // if sadashiv paul is there then in place of space browser use %20 by default and removing the space
+
+// console.log(url.replace('%20','_'));
+
+// console.log(url.includes('sadashiv')); // includes check krta hai word string me hai ki nhi hai
+
+// console.log(url.includes('chutiya'));
+
+
+// console.log(gameName.split('-'));
+
+  
+  
--- .devcontainer/Basics/datatypes_summery.js
+++ .devcontainer/Basics/datatypes_summery.js
@@ -38,9 +38,37 @@
 // console.log(detail.name);
 
 //Functions
-const myfunction = function()
-{
-    console.log("Hello World");
+// const myfunction = function()
+// {
+//     console.log("Hello World");
     
-}
-console.log(typeof(myfunction));
+// }
+// console.log(typeof(myfunction));
+
+//++++++++++++++++++++++++++Stack & Heap+++++++++++++++++++++++++++++++++
+//memeory can be defined in two ways
+
+//1.stack(Primitive), 2.Heap(Non-Primitive)
+//  let myYoutubename= "sadashivpaul.com"
+
+
+//  let anothername = myYoutubename
+//  anothername="chaiaurcode"
+// console.log(myYoutubename);
+
+//  console.log(anothername);
+
+ let user1={
+    email:"user@email.com",
+    upi:"user@ybl"
+ }
+ 
+ let user2 = user1
+ 
+
+ user2.email="sadashiv@google.com"
+
+ console.log(user1.email);
+ console.log(user2.email);
+ 
+ 
